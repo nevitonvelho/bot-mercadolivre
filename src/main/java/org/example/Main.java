81e9a216fbdac8fc1875b8f2f992ac2e455b5a93
nevitonvelho <nevitonvelho01@gmail.com>
2024-10-22 23:52:17 -0300

    start-maximixed funcional

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,23 +1,31 @@
 package org.example;
+
 import com.microsoft.playwright.*;
+
 import javax.swing.JOptionPane;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-
         try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-            Page page = browser.newPage();
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
+                    .setHeadless(false)
+                    .setArgs(List.of("--start-maximized")));
 
-            page.navigate("http://mercadolivre.com.br");
+            BrowserContext context = browser.newContext(new Browser.NewContextOptions()
+                    .setViewportSize(null));
 
+            Page page = context.newPage();
 
+            page.navigate("https://www.mercadolivre.com.br");
+
+            page.getByLabel("Digite o que você quer encontrar").fill("Celular");
+            page.keyboard().press("Enter");
 
             JOptionPane.showMessageDialog(null, "Clique em OK para fechar o navegador e finalizar o programa.");
 
             browser.close();
-
         }
     }
 }
